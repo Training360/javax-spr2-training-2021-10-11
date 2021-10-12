01ea998fcb1adf3107574cac327f425f32402f48
Istvan Viczian <viczian.istvan@gmail.com>
2021-10-12 13:37:29 +0200

    H2 + Spring Data JPA

--- src/main/java/employees/Employee.java
+++ src/main/java/employees/Employee.java
@@ -4,13 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "employees")
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "emp_name", nullable = false)
     private String name;
 
     public Employee(String name) {
