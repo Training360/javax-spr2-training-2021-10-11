2bd3cf04821871c0769f3c6339186afefcaa9dc8
Istvan Viczian <viczian.istvan@gmail.com>
2021-10-18 09:54:36 +0200

    Spring Security bevezetése

--- src/main/java/employees/NotFoundException.java
+++ src/main/java/employees/NotFoundException.java
@@ -1,14 +1,20 @@
 package employees;
 
-import org.zalando.problem.AbstractThrowableProblem;
-import org.zalando.problem.Status;
+//import org.zalando.problem.AbstractThrowableProblem;
+//import org.zalando.problem.Status;
 
 import java.net.URI;
 
-public class NotFoundException extends AbstractThrowableProblem {
+public class NotFoundException extends RuntimeException {
 
     public NotFoundException(String message) {
-        super(URI.create("not-found"), "Not found", Status.NOT_FOUND, message);
+        super(message);
     }
-
 }
+//extends AbstractThrowableProblem {
+//
+//    public NotFoundException(String message) {
+//        super(URI.create("not-found"), "Not found", Status.NOT_FOUND, message);
+//    }
+//
+//}
