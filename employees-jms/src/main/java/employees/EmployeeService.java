de9a9b5c7ddf98774a1a7082ff0085910f207835
Istvan Viczian <viczian.istvan@gmail.com>
2021-10-19 13:32:43 +0200

    JMS kiindulási projekt

---
+++ employees-jms/src/main/java/employees/EmployeeService.java
@@ -0,0 +1,70 @@
+package employees;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+//import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Optional;
+//import java.util.stream.IntStream;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    private EmployeeConverter employeeConverter;
+
+//    @PostConstruct
+//    public void init() {
+//        IntStream.range(0, 100)
+//                .forEach(i -> createEmployee(new CreateEmployeeCommand("John Doe " + i)));
+//
+//    }
+
+    public List<EmployeeDto> listEmployees(Optional<String> part, Pageable pageable) {
+
+        if (!part.isPresent()) {
+            return employeeRepository.findAllOrderByName(pageable);
+        }
+        else {
+            return employeeRepository.findAllByName("%" + part.get() + "%", pageable);
+        }
+
+    }
+
+    public EmployeeDto findEmployeeById(long id) {
+        return employeeConverter.convert(employeeRepository.findById(id)
+                        .orElseThrow(() -> new NotFoundException("Employee not found with id: " + id)));
+
+    }
+
+    public EmployeeDto createEmployee(CreateEmployeeCommand command) {
+
+        Employee employee = new Employee(command.getName());
+        employeeRepository.save(employee);
+        return employeeConverter.convert(employee);
+
+    }
+
+    @Transactional
+    public EmployeeDto updateEmployee(long id, UpdateEmployeeCommand command) {
+        Employee employeeToModify = employeeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Employee not found with id: " + id));
+        employeeToModify.setName(command.getName());
+        return employeeConverter.convert(employeeToModify);
+
+    }
+
+    public void deleteEmployee(long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Employee not found with id: " + id));
+        employeeRepository.delete(employee);
+
+    }
+}
---
+++ employees-jms/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into employees(emp_name) values ('John Doe');
+insert into employees(emp_name) values ('Jack Doe');
+insert into employees(emp_name) values ('Jane Doe');
